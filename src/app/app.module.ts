29ca19b308bbcd07943b65e58721064169bc88d5
lufeng111 <1836232210@qq.com>
2019-07-16 04:00:13 +0800

    依赖注入详解

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Product1Component } from './product1/product1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Product1Component
   ],
   imports: [
     BrowserModule,
